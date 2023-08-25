69b731dc3f4e2c65a676169a598e64b9c0ecd44b
Peter Ze <peterze@Peters-Air.lan>
2023-08-24 21:56:45 -0400

    refractor: mern vite

---
+++ MLH-web3/server/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+// import { routes } from './routes';
+
+const app = express();
+dotenv.config();
+
+// routes
+// app.use('/', routes);
+
+// start the server
+app.listen(process.env.BACK_PORT, () => {
+  console.log(
+    `server running : http://${process.env.BACK_HOST}:${process.env.BACK_PORT}`
+  );
+});
